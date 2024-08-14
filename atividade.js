615201e51269700d59bb2415c3f3676a45970711
gayzao <163845266+Pietro2024@users.noreply.github.com>
2024-08-14 07:46:40 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,14 @@
+console.log("Escreva Seu nome:")
+const Nome = pietro
+
+console.log("Escreva Seu Email:")
+const Email = pietro.camillo.oliveira
+
+console.log("Escreva Seu Idade:")
+const Idade = 16
+
+console.log("Escreva Seu Endereço:")
+
+console.log("Escreva Seu sexo:")
+
+console.log("Escreva Seu profissão:")
